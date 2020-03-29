882b1b81f495803e3394797a45b34a17a7a1b62d
Graeme Hill <Graeme.Hill@empired.com>
2020-03-29 20:33:36 +0800

    Initial Version

---
+++ ArduinoMorseBeaconKeyer.h
@@ -0,0 +1,11 @@
+/*
+  ArduinoMorseBeaconKeyer.h - Library for Morse Code Beacon Keyer.
+  Created by Graeme Hill, VK6HIL, Mar 2020
+  Released into the public domain.
+*/
+#ifndef ArduinoMorseBeaconKeyer_h
+#define ArduinoMorseBeaconKeyer_h
+
+#include "Arduino.h"
+
+#endif
---
+++ ArduinoMorseBeaconKeyer.ino
@@ -0,0 +1,16 @@
+/************************************************************************
+  Switches Antennas
+  Note : Should really use the reference analogue output value as the
+  band voltage does seem to swing about quite a lot
+*************************************************************************/
+
+
+#include "ArduinoMorseBeaconKeyer.h"
+
+
+void setup()
+{
+}
+void loop()
+{
+}
